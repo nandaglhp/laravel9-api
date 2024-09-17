b67a2a2681a2cb09df946dda2d4bc157877151f8
Ananda Galih Pratiwi <nandaglhp@gmail.com>
2024-09-17 14:50:00 +0700

    upload-data-to-db

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource; // data --> json
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage; // hapus gambar dari server
 
 class PostController extends Controller
 {
@@ -75,4 +76,51 @@ class PostController extends Controller
         // return single post as a resource
         return new PostResource(true, 'Data Post Ditemukan!', $post);
       }
+
+    /**
+     * update
+     *
+     * @param mixed $request
+     * @param mixed $post
+     * @return void
+     */
+
+     public function update(Request $request, Post $post){
+        // define validation rules
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        // check if validation fails
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        // check if image is not empty
+        if($request->hasFile('image')){
+            // upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            // delete old image
+            Storage::delete('public/posts/' .$post->image);
+
+            // update post with new image
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        } else {
+            // update post without image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+
+        // return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
+     }
 }
